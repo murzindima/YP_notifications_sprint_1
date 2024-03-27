bfd3fae476675838977115e4adbeb9914d3e11ca
Dmitry Murzin <dmitry.murzin@exness.com>
2024-03-28 01:18:17 +0200

    WIP

--- notifications_api/src/api/v1/notifications.py
+++ notifications_api/src/api/v1/notifications.py
@@ -1,3 +1,6 @@
+import json
+
+import aio_pika
 from fastapi import APIRouter, Depends, status, HTTPException
 
 from src.schemas.notification import Notification as NotificationSchema
@@ -27,5 +30,25 @@ async def create_notification(
     notification.template_rendered = template_rendered
 
     notification = await notification_service.create_model(notification)
+
+    connection = await aio_pika.connect_robust("amqp://user:password@rabbitmq/")
+    async with connection:
+        channel = await connection.channel()
+
+        exchange = await channel.declare_exchange('emails', aio_pika.ExchangeType.DIRECT)
+        queue = await channel.declare_queue('welcome', durable=True)
+
+        await queue.bind(exchange, 'welcome')
+
+        message = aio_pika.Message(
+            body=json.dumps({
+                "user_id": 1,
+                "clicked_element": "button1",
+                "click_time": "10:30:00"
+            }).encode(),
+            delivery_mode=aio_pika.DeliveryMode.PERSISTENT
+        )
+
+        await exchange.publish(message, routing_key='welcome')
 
     return notification
