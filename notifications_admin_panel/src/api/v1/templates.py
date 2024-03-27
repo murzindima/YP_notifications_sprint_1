63ec25deca2ebf60618b9471ffd6f4821b2e4d64
Dmitry Murzin <dmitry.murzin@exness.com>
2024-03-27 20:42:02 +0200

    WIP

---
+++ notifications_admin_panel/src/api/v1/templates.py
@@ -0,0 +1,36 @@
+from http import HTTPStatus
+
+from fastapi import APIRouter, Depends, HTTPException
+
+from core.messages import TEMPLATE_NOT_FOUND, TEMPLATES_NOT_FOUND
+from queries.template import TemplateFilter, SearchTemplateFilter
+from schemas.template import Template as TemplateSchema
+from services.template import TemplateService, get_template_service
+
+router = APIRouter()
+
+
+@router.get("/", response_model=list[TemplateSchema])
+async def all_templates(
+    template_service: TemplateService = Depends(get_template_service),
+    template_filter: TemplateFilter = Depends(),
+) -> list[TemplateSchema]:
+    """Returns all templates with pagination."""
+    templates = await template_service.get_all_models(template_filter)
+
+    if not templates:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=TEMPLATES_NOT_FOUND)
+
+    return [TemplateSchema(**template.model_dump()) for template in templates]
+
+
+@router.get("/{template_id}", response_model=TemplateSchema)
+async def template_details(
+    template_id: str, template_service: TemplateService = Depends(get_template_service)
+) -> TemplateSchema:
+    """Returns the template by identifier."""
+    template = await template_service.get_model_by_id(template_id)
+    if not template:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=TEMPLATE_NOT_FOUND)
+
+    return TemplateSchema(**template.model_dump())
---
+++ notifications_admin_panel/src/main.py
@@ -0,0 +1,74 @@
+import logging
+
+import uvicorn
+from fastapi import FastAPI, Request, status, Depends
+from fastapi.responses import ORJSONResponse
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from pydantic import ValidationError
+
+from api.v1 import films
+from core import config
+from core.logger import LOGGING
+from middleware.external_auth import security_jwt
+
+
+def configure_tracer() -> None:
+    resource = Resource(attributes={
+        SERVICE_NAME: "content-delivery-service"
+    })
+    trace.set_tracer_provider(TracerProvider(resource=resource))
+    trace.get_tracer_provider().add_span_processor(
+        BatchSpanProcessor(
+            JaegerExporter(
+                agent_host_name=config.jaeger_settings.host,
+                agent_port=config.jaeger_settings.port,
+            )
+        )
+    )
+
+
+configure_tracer()
+
+
+app = FastAPI(
+    title=config.app_settings.project_name,
+    description="Notifications Admin Panel",
+    docs_url="/api/openapi",
+    openapi_url="/api/openapi.json",
+    default_response_class=ORJSONResponse,
+)
+
+FastAPIInstrumentor.instrument_app(app)
+
+
+@app.middleware('http')
+async def before_request(request: Request, call_next):
+    response = await call_next(request)
+    request_id = request.headers.get('X-Request-Id')
+    if not request_id:
+        return ORJSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={'detail': 'X-Request-Id is required'})
+    return response
+
+app.include_router(films.router, prefix="/api/v1/templates", tags=["templates"], dependencies=[Depends(security_jwt)])
+
+
+@app.exception_handler(ValidationError)
+async def handler_validation_error(request: Request, exc: ValidationError):
+    return ORJSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST, content={"errors": exc.errors()}
+    )
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        log_config=LOGGING,
+        log_level=logging.DEBUG,
+    )
